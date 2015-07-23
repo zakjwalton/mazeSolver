f688dda35a7affb5dafe3476f71f155bb6a65637
Nick Schrock <nick.a.schrock@gmail.com>
2015-07-24 00:59:27 +0100

    Added brute force method to maze solver

--- mazeSolver.cpp
+++ mazeSolver.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stack>
+
 
 using namespace std;
 
@@ -11,6 +13,7 @@ int greedyMazeSolver(int i, int j);
 int divideAndConquerMazeSolver(int i, int j);
 int dynamicProgrammingMazeSolver(int i, int j);
 int randomizedMazeSolver(int i, int j);
+
 
 struct maze
 {
@@ -25,7 +28,7 @@ int main()
 {
 	//required variables
 	ifstream in;
-	in.open("maze.txt");
+	in.open("sample_mazes/CSSmaze2.txt");
 	char line;
 	
 	//read the matrix using plain c code, character by character
@@ -90,7 +93,132 @@ int main()
 
 int bruteForceMazeSolver(int i, int j)
 {
-    //add algorithm here
+    cout << "in method" << endl;
+
+    int curr_x = i;
+    int curr_y = j;
+    int count = 0;
+    stack<char> moves;
+
+    while(myMaze.matrix[curr_x][curr_y] != 'F'){
+
+
+
+    	//try going right
+    	if(myMaze.matrix[curr_x][curr_y+1] == ' ' || myMaze.matrix[curr_x][curr_y+1] == 'F' ){
+    		cout << "right" << endl;
+    		count ++;
+    		curr_y++;
+    		if(myMaze.matrix[curr_x][curr_y] == 'F'){
+    			break;
+    		}
+    		moves.push('r');
+    		myMaze.matrix[curr_x][curr_y] = 'o';
+    	}
+
+    	//try going up
+    	else if(myMaze.matrix[curr_x-1][curr_y] == ' ' || myMaze.matrix[curr_x-1][curr_y] == 'F'){
+    		cout << "up" << endl;
+    		count++;
+    		//break;
+    		curr_x--; 
+    		if(myMaze.matrix[curr_x][curr_y] == 'F'){
+    			break;
+    		}   
+    		moves.push('u');
+    		myMaze.matrix[curr_x][curr_y] = 'o';   		
+    	}
+
+    	//try going down
+    	else if(myMaze.matrix[curr_x+1][curr_y] == ' ' || myMaze.matrix[curr_x+1][curr_y] == 'F'){
+    		cout << "down" << endl;
+    		count++;
+    		curr_x++;
+    		if(myMaze.matrix[curr_x][curr_y] == 'F'){
+    			break;
+    		}
+    		moves.push('d');
+    		myMaze.matrix[curr_x][curr_y] = 'o';
+    		
+    	}
+
+    	//try going left
+    	else if (myMaze.matrix[curr_x][curr_y-1] == ' ' || myMaze.matrix[curr_x][curr_y-1] == 'F'){
+    		count++;
+    		cout << "left" << endl;
+    		//break;
+    		curr_y--;
+    		if(myMaze.matrix[curr_x][curr_y] == 'F'){
+    			break;
+    		}
+    		moves.push('l');
+			myMaze.matrix[curr_x][curr_y] = 'o';
+    	}
+    	
+
+    	//try backtracking right
+    	else if (moves.top() == 'l'){
+    		moves.pop();
+    		count++;
+    		if(myMaze.matrix[curr_x][curr_y] != 'S'){
+    			myMaze.matrix[curr_x][curr_y] = 'x';
+    		}
+    		curr_y++;
+    		cout << "backtracked right" << endl;
+    	}
+
+    	//try backtracking up 
+    	else if (moves.top() == 'd'){
+    		moves.pop();
+    		count++;
+    		if(myMaze.matrix[curr_x][curr_y] != 'S'){
+    			myMaze.matrix[curr_x][curr_y] = 'x';
+    		}
+    		curr_x--;
+    		cout << "backtracked up" << endl;
+    	}
+
+    	//try backtracking down 
+    	else if (moves.top() == 'u'){
+    		moves.pop();
+    		count++;
+    		if(myMaze.matrix[curr_x][curr_y] != 'S'){
+    			myMaze.matrix[curr_x][curr_y] = 'x';
+    		}
+    		curr_x++;
+    		cout << "backtracked down" << endl;
+    	}
+
+    	//try backtracking left
+    	else if (moves.top() == 'r'){
+    		moves.pop();
+    		count++;
+    		if(myMaze.matrix[curr_x][curr_y] != 'S'){
+    			myMaze.matrix[curr_x][curr_y] = 'x';
+    		}
+    		curr_y--;
+    		cout << "backtracked left" << endl;
+    	}
+
+    	else{
+    		cout << "Error: Maze Unsolvable" << endl;
+    		break;
+    	}
+
+    	/*if(count > 118){
+    		break;
+    	}*/
+    }
+
+    //Clear X's
+    	for(int k=0; k<myMaze.rows; k++){
+    		for(int h=0; h<myMaze.cols; h++){
+    			if(myMaze.matrix[k][h] == 'x'){
+    				myMaze.matrix[k][h] = ' ';
+    			}
+    		}
+    	}
+
     return -1;    
 }
 int backtrackingMazeSolver(int i, int j)
